ae5dc31455593fb306b9e57ceec3eeecce830b89
Renata Floro <flororenataef@gmail.com>
2021-12-13 22:41:24 -0300

    post ok-parte inicial get

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -14,14 +14,26 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
-  
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token),
+  };
 
+  refreshToken(){
+    this.token = {
+      headers: new HttpHeaders().set('Authorization', environment.token)
+    }
+  }
+  
   cadastrar(usuario: Usuario): Observable<Usuario>{
     return this.http.post<Usuario>('http://localhost:8080/usuario/cadastrar', usuario)
   }
 
   entrar(userLogin: UserLogin): Observable<UserLogin>{ 
     return this.http.post<UserLogin>('http://localhost:8080/usuario/logar', userLogin)
+  }
+
+  getByIdUser(id:number):Observable<Usuario>{
+    return this.http.get<Usuario>(`http://localhost:8080/usuario/${id}`,this.token)
   }
 
   logado(){
